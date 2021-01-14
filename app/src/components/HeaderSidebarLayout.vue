e4ead04edfc73972a8d0d1e0b9e77ba0fd8f8b9d
Sam <samstern@google.com>
2021-01-14 15:38:31 +0000

    Introduce HeaderSidebarLayout

---
+++ app/src/components/HeaderSidebarLayout.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="grid grid-cols-12 gap-0">
+    <!-- Side bar -->
+    <div class="col-span-2 bg-gray-50 py-10 px-10">
+      <slot name="sidebar"></slot>
+    </div>
+
+    <div class="col-span-10">
+      <!-- Header -->
+      <div>
+        <slot name="header"></slot>
+      </div>
+
+      <!-- Body -->
+      <div class="mb-20">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  components: {},
+})
+export default class HeaderSidebarLayout extends Vue {}
+</script>
+
+<style scoped lang="postcss"></style>
--- app/src/views/Product.vue
+++ app/src/views/Product.vue
@@ -1,33 +1,6 @@
 <template>
-  <div class="grid grid-cols-12 gap-0">
-    <!-- Side bar -->
-    <div class="col-span-2 bg-gray-50 py-10 px-10">
-      <p class="uppercase font-medium mt-4 mb-2">Sort</p>
-
-      <RadioGroup
-        prefix="sort"
-        :keys="['Recently Updated', 'Most Popular', 'Trending Now']"
-        :values="['updated', 'popular', 'trending']"
-      />
-
-      <p class="uppercase font-medium mt-4 mb-2">Type</p>
-
-      <CheckboxGroup
-        prefix="type"
-        :keys="['All', 'Open Source', 'Blog Posts']"
-        :values="['all', 'open-source', 'blog']"
-      />
-
-      <p class="uppercase font-medium mt-4 mb-2">Category</p>
-
-      <CheckboxGroup
-        prefix="category"
-        :keys="['All', 'Android', 'iOS', 'Web', 'Games', 'Admin']"
-        :values="['all', 'android', 'ios', 'web', 'games', 'admin']"
-      />
-    </div>
-
-    <div class="col-span-10">
+  <HeaderSidebarLayout>
+    <template v-slot:header>
       <!-- Header -->
       <div class="bg-firebase-bg py-20 grid grid-cols-10 gap-4">
         <div class="col-start-2 col-span-5 text-white">
@@ -51,33 +24,62 @@
           </div>
         </div>
       </div>
+    </template>
 
-      <!-- Body -->
-      <div class="grid grid-cols-10 gap-4 mb-20">
-        <div class="col-start-2 col-span-8">
-          <p class="font-display text-2xl mt-8">Open Source</p>
+    <template v-slot:sidebar>
+      <!-- Side bar -->
+      <div>
+        <p class="uppercase font-medium mt-4 mb-2">Sort</p>
 
-          <div class="grid grid-cols-2 gap-4">
-            <LargeProjectCard class="mt-4" />
-            <LargeProjectCard class="mt-4" />
+        <RadioGroup
+          prefix="sort"
+          :keys="['Recently Updated', 'Most Popular', 'Trending Now']"
+          :values="['updated', 'popular', 'trending']"
+        />
 
-            <LargeProjectCard class="mt-4" />
-            <LargeProjectCard class="mt-4" />
-          </div>
+        <p class="uppercase font-medium mt-4 mb-2">Type</p>
 
-          <p class="font-display text-2xl mt-8">Blog Posts</p>
+        <CheckboxGroup
+          prefix="type"
+          :keys="['All', 'Open Source', 'Blog Posts']"
+          :values="['all', 'open-source', 'blog']"
+        />
 
-          <div class="grid grid-cols-2 gap-4">
-            <LargeProjectCard class="mt-4" />
-            <LargeProjectCard class="mt-4" />
+        <p class="uppercase font-medium mt-4 mb-2">Category</p>
 
-            <LargeProjectCard class="mt-4" />
-            <LargeProjectCard class="mt-4" />
-          </div>
+        <CheckboxGroup
+          prefix="category"
+          :keys="['All', 'Android', 'iOS', 'Web', 'Games', 'Admin']"
+          :values="['all', 'android', 'ios', 'web', 'games', 'admin']"
+        />
+      </div>
+    </template>
+
+    <!-- Body -->
+    <div class="grid grid-cols-10 gap-4 mb-20">
+      <div class="col-start-2 col-span-8">
+        <p class="font-display text-2xl mt-8">Open Source</p>
+
+        <div class="grid grid-cols-2 gap-4">
+          <LargeProjectCard class="mt-4" />
+          <LargeProjectCard class="mt-4" />
+
+          <LargeProjectCard class="mt-4" />
+          <LargeProjectCard class="mt-4" />
+        </div>
+
+        <p class="font-display text-2xl mt-8">Blog Posts</p>
+
+        <div class="grid grid-cols-2 gap-4">
+          <LargeProjectCard class="mt-4" />
+          <LargeProjectCard class="mt-4" />
+
+          <LargeProjectCard class="mt-4" />
+          <LargeProjectCard class="mt-4" />
         </div>
       </div>
     </div>
-  </div>
+  </HeaderSidebarLayout>
 </template>
 
 <script lang="ts">
@@ -86,6 +88,7 @@ import MaterialButton from "@/components/MaterialButton.vue";
 import LargeProjectCard from "@/components/LargeProjectCard.vue";
 import RadioGroup from "@/components/RadioGroup.vue";
 import CheckboxGroup from "@/components/CheckboxGroup.vue";
+import HeaderSidebarLayout from "@/components/HeaderSidebarLayout.vue";
 
 @Component({
   components: {
@@ -93,6 +96,7 @@ import CheckboxGroup from "@/components/CheckboxGroup.vue";
     LargeProjectCard,
     RadioGroup,
     CheckboxGroup,
+    HeaderSidebarLayout,
   },
 })
 export default class Product extends Vue {}
