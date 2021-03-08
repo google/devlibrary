3026eb38d189280e1f3f5e92eac2429ff17fedd7
Sam <samstern@google.com>
2021-03-08 16:58:32 +0000

    Drop route splitting

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
 import Home from "../views/Home.vue";
+import About from "../views/About.vue";
 import Product from "../views/Product.vue";
 import Repo from "../views/Repo.vue";
 
@@ -30,11 +31,7 @@ const routes: Array<RouteConfig> = [
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    component: About,
   },
 ];
 
