67fff0717beea05b557a6a3d39f82b2bcfc540fb
Sam Stern <samstern@google.com>
2021-12-20 11:23:17 +0000

    Fix diagnose script

--- shared/scripts/diagnoserepo.ts
+++ shared/scripts/diagnoserepo.ts
@@ -4,22 +4,9 @@ import * as path from "path";
 import { RepoMetadata } from "../types/RepoMetadata";
 import { getRepoContent, getRepoLicense } from "./addproject";
 
-export async function main(args: string[]) {
-  if (args.length < 2) {
-    console.error(
-      "Missing required arguments:\nnpm run auditrepo <config file path>"
-    );
-    return;
-  }
-
-  const configFilePath = args[2];
-  const absPath = path.resolve(configFilePath);
-
-  console.log(`File ${absPath}`);
-  const fileContent = fs.readFileSync(absPath).toString();
-  const repoConfig = JSON.parse(fileContent) as RepoMetadata;
-
-  const { owner, repo, content } = repoConfig;
+export async function diagnoseRepo(metadata: RepoMetadata) {
+  const { owner, repo, content } = metadata;
+  console.log();
   console.log(`Repo: https://github.com/${owner}/${repo}`);
   console.log();
 
@@ -30,15 +17,15 @@ export async function main(args: string[]) {
     if (key === "apache-2.0" || key === "mit") {
       console.log(`✅ Valid license type: ${key}`);
     } else {
-      console.log(`❌ Error: repository has invalid license type: ${key}`);
+      console.log(`❌ Error: ${owner}/${repo} has invalid license type: ${key}`);
     }
   } else {
-    console.log("❌ Error: repository does not have a license.");
+    console.log(`❌ Error: ${owner}/${repo} does not have a license.`);
   }
 
   // 2) Check that the content paths actually exist
   const contentPaths = [content];
-  const pages = repoConfig.pages || [];
+  const pages = metadata.pages || [];
   for (const p of pages) {
     contentPaths.push(p.path);
   }
@@ -48,8 +35,26 @@ export async function main(args: string[]) {
     if (pageContent) {
       console.log(`✅ Valid content ${p}`);
     } else {
-      console.log(`❌ Error: invalid content path ${p}`);
+      console.log(`❌ Error: ${owner}/${repo} has no content path ${p}`);
     }
+  }
+}
+
+export async function main(args: string[]) {
+  if (args.length < 3) {
+    console.error(
+      "Missing required arguments:\nnpm run diagnoserepo <config file path> <... more paths>"
+    );
+    return;
+  } 
+
+  for (let i = 2; i < args.length; i++) {
+    const configFilePath = args[i];
+    const absPath = path.resolve(configFilePath);
+  
+    const fileContent = fs.readFileSync(absPath).toString();
+    const metadata = JSON.parse(fileContent) as RepoMetadata;
+    await diagnoseRepo(metadata);
   }
 }
 
