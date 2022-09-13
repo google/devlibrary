40721cf1a6900c4dfc131b00f4deb52961b39294
barnesjoseph <josephbarnes@google.com>
2022-09-13 15:24:46 -0700

    Adds dynamic author image to /authors

--- app/src/assets/ts/profile-colors.ts
+++ app/src/assets/ts/profile-colors.ts
@@ -1,66 +1,66 @@
 export const ColorJson = [
-    {
-        "background": "#f44336",
-        "color": "black"
-    },
-    {
-        "background": "#e91e63",
-        "color": "black"
-    },
-    {
-        "background": "#9c27b0",
-        "color": "white"
-    },
-    {
-        "background": "#673ab7",
-        "color": "white"
-    },
-    {
-        "background": "#3f51b5",
-        "color": "white"
-    },
-    {
-        "background": "#2196f3",
-        "color": "black"
-    },
-    {
-        "background": "#03a9f4",
-        "color": "black"
-    },
-    {
-        "background": "#00bcd4",
-        "color": "black"
-    },
-    {
-        "background": "#009688",
-        "color": "black"
-    },
-    {
-        "background": "#4caf50",
-        "color": "black"
-    },
-    {
-        "background": "#8bc34a",
-        "color": "black"
-    },
-    {
-        "background": "#cddc39",
-        "color": "black"
-    },
-    {
-        "background": "#ffeb3b",
-        "color": "black"
-    },
-    {
-        "background": "#ffc107",
-        "color": "black"
-    },
-    {
-        "background": "#ff9800",
-        "color": "black"
-    },
-    {
-        "background": "#ff5722",
-        "color": "black"
-    }
+	{
+		"background": "#f44336",
+		"color": "black"
+	},
+	{
+		"background": "#e91e63",
+		"color": "black"
+	},
+	{
+		"background": "#9c27b0",
+		"color": "white"
+	},
+	{
+		"background": "#673ab7",
+		"color": "white"
+	},
+	{
+		"background": "#3f51b5",
+		"color": "white"
+	},
+	{
+		"background": "#2196f3",
+		"color": "black"
+	},
+	{
+		"background": "#03a9f4",
+		"color": "black"
+	},
+	{
+		"background": "#00bcd4",
+		"color": "black"
+	},
+	{
+		"background": "#009688",
+		"color": "black"
+	},
+	{
+		"background": "#4caf50",
+		"color": "black"
+	},
+	{
+		"background": "#8bc34a",
+		"color": "black"
+	},
+	{
+		"background": "#cddc39",
+		"color": "black"
+	},
+	{
+		"background": "#ffeb3b",
+		"color": "black"
+	},
+	{
+		"background": "#ffc107",
+		"color": "black"
+	},
+	{
+		"background": "#ff9800",
+		"color": "black"
+	},
+	{
+		"background": "#ff5722",
+		"color": "black"
+	}
 ];
