95869f44a8a36eb2f7cbad422c6f51a7eabfb7ba
barnesjoseph <josephbarnes@google.com>
2022-10-10 09:47:25 -0700

    Update About.vue

--- app/src/views/About.vue
+++ app/src/views/About.vue
@@ -106,106 +106,91 @@
         <div class="mt-6 lg:mt-10 lg:text-lg">
           <p class="mt-6 font-bold">What is Dev Library?</p>
           <p class="mt-4">
-            Dev Library is an open-source platform showcasing a developer’s work
-            with Google technologies. It hosts GitHub projects and Blogs from
-            external sites (Medium, Hashnode, Dev.to, personal sites) built
-            using any of the Google Technologies like:
+            Dev Library is a platform for showcasing open-source projects and
+            technical blogs featuring Google technologies.
           </p>
-          <ul class="ml-6 mt-2 list-disc">
-            <li>Angular</li>
-            <li>Android</li>
-            <li>Flutter</li>
-            <li>Firebase</li>
-            <li>Machine Learning</li>
-            <li>Cloud</li>
-            <li>Google Assistant</li>
-          </ul>
-
-          <p>
-            DevLibrary aims to provide visibility to developers by showcasing
-            their content. We hope this visibility helps them reach a wider
-            audience and inspire other developers to learn, contribute and share
-            their knowledge.
+          <p class="mt-2">
+            Any developer who has built an open-source project using Google's
+            technologies, or has written about a solution they solved using
+            Google's tools, can apply to have their project or content featured
+            on the site.
+          </p>
+          <p class="mt-2">
+            Google experts then review each submission for quality, relevance
+            and uniqueness. If approved, the project is included on the site.
           </p>
 
           <p class="mt-6 font-bold">What is the purpose of this site?</p>
           <p class="mt-4">
-            We want to enable developers all around the world to showcase their
-            use of Google technologies and for all developers to find
-            inspiration and tools to use in their own projects.
+            We noticed all of the amazing open-source projects being built with
+            Google's tools and technologies. We wanted to create a place that
+            would provide visibility for the talented developers behind the
+            projects, and introduce high-quality projects to a wide audience of
+            developers.
+          </p>
+          <p class="mt-2">
+            You can browse through the projects and content featured on the site
+            for inspiration, search for a particular product or keyword, or
+            submit your project for review and the possibility of being included
+            on the site.
           </p>
 
           <p class="mt-6 font-bold">
-            Will this remove the need for documentations and guides?
+            Are there any benefits to contributing to the site?
           </p>
           <p class="mt-4">
-            Not at all! This site is a complement to Google’s official
-            documentation. We hope you’ll find inspiring and useful pieces of
-            code built by developers worldwide.
-          </p>
-
-          <p class="mt-6 font-bold">Why did my submission get rejected?</p>
-          <p class="mt-4">
-            Submissions can get rejected due to various reasons. Some common
-            reasons for rejecting content are listed below:
+            What is hard work without any recognition! Recognizing talented
+            developers is one of the intended goals of this site. Contributors
+            are rewarded with:
           </p>
           <ul class="ml-6 mt-2 list-disc">
             <li>
-              <strong> Absence of a GitHub license </strong>
-              : Before submitting a project, ensure you have either added Apache
-              2.0 or MIT license.
+              Author page
             </li>
             <li>
-              <strong> Incoherent README </strong>
-              : Make sure you don’t have an empty README. To get accepted, we
-              encourage having a properly written, structured, and
-              well-explained README with visuals, if needed.
+              Contributor badge on their Google Developer Profile
             </li>
             <li>
-              <strong> Improper Versioning </strong>
-              : Make sure your project follows the latest software updates.
-            </li>
-            <li>
-              <strong> Monetised</strong>
-              : If in some way we find out that the blogs are behind a paywall
-              (in Medium) or have ads, or are getting monetized in any form, we
-              don’t accept them.
-            </li>
-            <li>
-              <strong> Localization</strong>
-              : Currently, because of being a fairly new platform–we don’t
-              support content in a language other than English. Although, we are
-              working toward localization–for now–it is not accepted.
-            </li>
-            <li>
-              <strong> Freshness</strong>
-              : If the content submitted by you is fairly older or hasn’t been
-              updated for a long time–chances of rejection are higher.
+              Feature in our monthly newsletter
             </li>
           </ul>
 
-          <p>
-            For an extensive explanation, read our
-            <a
-              href="https://devlibrary.advocu.com/assets/docs/contributors_guidelines.pdf"
-              >contributor guidelines</a
-            >
-            to fix the errors and re-submit.
+          <p class="mt-6 font-bold">
+            I want to learn how to code. Is this the right resource for me?
+          </p>
+          <p class="mt-4">
+            No. If you are completely new to computer programming, Google
+            <a href="https://grasshopper.app/">Grasshopper</a> teaches adult
+            learners to write JavaScript, free of charge.
+          </p>
+          <p class="mt-2">
+            If you're a beginner developer with some basic coding knowledge, we
+            encourage you to browse the site. You may learn something useful
+            from the open source projects and libraries.
           </p>
 
-          <p class="mt-6 font-bold">How to submit my content on the site?</p>
-          <p class="mt-4">To submit the content:</p>
+          <p class="mt-6 font-bold">
+            Will this remove the need for documentation and guides?
+          </p>
+          <p class="mt-4">
+            No. We hope you'll find inspiring and useful pieces of code built by
+            developers worldwide, but this site is not meant to replace Google's
+            official documentation.
+          </p>
+
+          <p class="mt-6 font-bold">
+            How do I submit my content to be featured on the site?
+          </p>
           <ol class="ml-6 mt-2 list-disc">
-            <li>Navigate to the DevLibrary homepage and click submit.</li>
+            <li>Find the Submit button on the DevLibrary Homepage.</li>
             <li>You will be redirected to the Advocu Form link.</li>
-            <li>Fill in your personal and project details.</li>
+            <li>Fill in your personal and content details.</li>
             <li>Click Submit.</li>
+            <li>
+              You will receive an email informing you that your submission has
+              been received. Note: Once submitted, you can't edit the form.
+            </li>
           </ol>
-
-          <p>
-            You will receive an email once your submission has been curated.
-            <b>Note</b>: Once submitted, you can’t edit the form.
-          </p>
 
           <p class="mt-6 font-bold">
             How many submissions can an individual make?
@@ -216,98 +201,105 @@
           </p>
 
           <p class="mt-6 font-bold">
-            What is the process of selection/rejection of content?
+            What happens after I submit?
           </p>
           <p class="mt-4">
-            Once you make the submission of your work (GitHub or Blog), internal
-            Google experts from each product area review it. Check it for
-            quality, freshness, and uniqueness. If it passes all the checks,
-            they select the content to go on the site. You then receive an email
-            about the acceptance of your work.
+            Google experts from each product area review submissions for
+            quality, freshness, and uniqueness. If it is deemed appropriate,
+            they will approve the content to be featured on the site. You will
+            then receive an email about the acceptance of your work.
           </p>
 
-          <p class="mt-6 font-bold">How does a submission get selected?</p>
+          <p class="mt-6 font-bold">Why did my submission get rejected?</p>
           <p class="mt-4">
-            Each submission will be reviewed by Google product experts to
-            determine if it is high-quality, relevant, and unique.
+            Submissions are rejected for various reasons, such as:
+          </p>
+          <ul class="ml-6 mt-2 list-disc">
+            <li>
+              <strong>Absence of a GitHub license</strong>:
+              : Before submitting a project, ensure you have either an Apache
+              2.0 or MIT license.
+            </li>
+            <li>
+              <strong>Incoherent README</strong>:
+              Make sure you don't have an empty README. To get accepted, we
+              encourage having a properly written, structured, and
+              well-explained README with visuals, if needed.
+            </li>
+            <li>
+              <strong>Improper Versioning</strong>:
+              Make sure your project follows the latest software updates.
+            </li>
+            <li>
+              <strong>Monetised</strong>:
+              If we find out that the blogs are behind a paywall (in Medium),
+              have ads, or are getting monetized in any form, we don’t accept
+              them.
+            </li>
+            <li>
+              <strong>Language</strong>:
+              Although we are working toward localization, we currently can only
+              support content in English.
+            </li>
+            <li>
+              <strong>Freshness</strong>:
+              If the content is fairly old, or hasn’t been updated for a long
+              time, chances of rejection are higher.
+            </li>
+          </ul>
+
+          <p class="mt-2">
+            For an extensive explanation, read our
+            <a
+              href="https://devlibrary.advocu.com/assets/docs/contributors_guidelines.pdf"
+              >contributor guidelines</a
+            >
+            to fix the errors and re-submit.
           </p>
 
           <p class="mt-6 font-bold">
-            When will I get updates about the rejection/acceptance of my
-            content?
+            When will I get updates about my submission?
           </p>
           <p class="mt-4">
-            As soon as you submit your content, you receive an email from the
-            Advocu team confirming your submission. Curators generally take 1-2
-            weeks to curate the content. Once done, you receive an email for the
-            Acceptance/Rejection of your work. If you don’t see an email, make
-            sure to check your spam or promotions folder. Also, allowlist
-            DevLibrary emails to refrain from missing out on the updates from
-            the next time onward.
+            As soon as you submit your content, you will receive an email
+            onfirming your submission. From there, it generally takes 1-2 weeks
+            for your content to be reviewed. After the review, you will receive
+            an email stating whether the project was accepted or not. If you
+            don’t see an email, make sure to check your spam or promotions
+            folder. Add DevLibrary emails to your allowlist so you don’t miss
+            out on the updates.
           </p>
 
           <p class="mt-6 font-bold">
-            How long does it take for content to appear on the site?
+            I was approved! How long will it take for my content to appear on
+            the site?
           </p>
           <p class="mt-4">
-            Once approved, the content gets on the site within a week of
-            approval.
+            Once approved, the content generally appears on the site within one
+            week.
           </p>
 
           <p class="mt-6 font-bold">
             Can I submit content in different product categories?
           </p>
-          <p class="mt-4">Yes! YOU CAN.</p>
-
-          <p class="mt-6 font-bold">
-            Is there any reward or recognition for contributors?
-          </p>
-          <p class="mt-4">
-            What is hard work without any recognition! If you become an author
-            at Dev Library, you receive:
-          </p>
-          <ul class="ml-6 mt-2 list-disc">
-            <li>Visibility of your work</li>
-            <li>Contributor Badge on Google Developer Profile</li>
-            <li>Stamp of Google on your work</li>
-            <li>Feature in our monthly newsletter</li>
-          </ul>
-
-          <p>And many more surprises!</p>
+          <p class="mt-4">Yes!</p>
 
           <p class="mt-6 font-bold">
             How do I request for content to be removed?
           </p>
           <p class="mt-4">
             To remove your content, please raise a Pull Request on the Dev
-            Library’s GitHub repo. In case you are unable to do this, please
-            fill out the
-            <a href="https://forms.gle/MdMEj7hkdWFLUBrf7" target="blank">
-              Contact Us form
-            </a>
-            and we will get back to you in 48 hours for next steps.
+            Library’s
+            <a href="https://github.com/google/devlibrary">GitHub repo</a>.
           </p>
 
-          <p class="mt-6 font-bold">How do I raise an issue?</p>
-          <p class="mt-4">
-            To raise an issue, please post your issue here on the Dev Library
-            repo. In case you are unable to do this, please fill out the
-            <a href="https://forms.gle/MdMEj7hkdWFLUBrf7" target="blank">
-              Contact Us form</a
-            >.
+          <p class="mt-6 font-bold">How do I raise another issue?</p>
+          <p class="mt-4 mb-20">
+            To raise an issue, post your issue on the
+            <a href="https://github.com/google/devlibrary">
+              Dev Library repo
+            </a>.
           </p>
-          <p class="mt-6 font-bold">
-            I want to learn how to code, is this the right resource for me?
-          </p>
-          <p class="mt-4">
-            If you have never coded yet, this is probably not the best site to
-            get started on. Instead type “learn JavaScript” or “learn Python” in
-            your favourite search engine :-) and you’ll find many excellent
-            resources. If you however have some basic coding knowledge, this
-            site may prove useful to you as it references curated open source
-            projects and libraries that you can re-use and learn from.
-          </p>
-          <p class="mt-20"></p>
         </div>
       </div>
     </div>
