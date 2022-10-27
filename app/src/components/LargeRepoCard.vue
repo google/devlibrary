36429ab478c716480eb9d0ce2ff47f5a8e593549
Usama Ijaz <usamaijaz@google.com>
2022-10-27 11:50:10 -0400

    Added functionality to redirect to authors profile when clicked on the logo

--- app/src/components/LargeRepoCard.vue
+++ app/src/components/LargeRepoCard.vue
@@ -20,15 +20,20 @@
   >
     <!-- Author photo and name -->
     <div class="frc">
-      <CircleImage
-        v-if="authorImageLoaded"
-        :lazy="true"
-        size="card-avatar"
-        class="mr-2"
-        :src="`https://avatars.githubusercontent.com/${repo.metadata.owner}`"
-      />
-      <div v-else v-html="dynamicAuthorImage"></div>
-      <span class="font-display text-lg">{{ repo.metadata.owner }}</span>
+      <!-- Link to author (if present) -->
+      <template v-if="authorId">
+        <router-link :to="`/authors/${authorId}`" class="frc">
+            <CircleImage
+              v-if="authorImageLoaded"
+              :lazy="true"
+              size="card-avatar"
+              class="mr-2"
+              :src="`https://avatars.githubusercontent.com/${repo.metadata.owner}`"
+            />
+            <div v-else v-html="dynamicAuthorImage"></div>
+            <span class="font-display text-lg">{{ repo.metadata.owner }}</span>
+        </router-link>
+      </template>
 
       <ProductLogo
         v-if="showLogo"
